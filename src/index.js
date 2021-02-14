f291b3d35b1e29883cfe69acaa84b4cadcfd2fba
tldms0327 <60428730+tldms0327@users.noreply.github.com>
2021-02-15 02:46:59 +0900

    add usePreventLeave

--- src/index.js
+++ src/index.js
@@ -17,15 +17,29 @@ const useConfirm = (message = "", callback, rejection) => {
   return confirmAction;
 };
 
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const protect = () => window.addEventListener("before ", listener);
+  const unprotect = () => window.removeEventListener("beforeUnload ", listener);
+  return { protect, unprotect };
+};
+
 const App = () => {
   const deleteWord = () => console.log("Deleting the word...");
   const abort = () => console.log("Aborted");
   const confirmDelete = useConfirm("Are you sure?", deleteWord, abort);
 
+  const { protect, unprotect } = usePreventLeave();
+
   return (
     <div className="App">
       <h1> Hooks </h1>
       <button onClick={confirmDelete}>Delete the word</button>
+      <button onClick={protect}>Protect</button>
+      <button onClick={unprotect}>Unprotect</button>
     </div>
   );
 };
