0e8f500513c42a68ba3512922661d8057c92d32a
tldms0327 <60428730+tldms0327@users.noreply.github.com>
2021-02-15 02:03:47 +0900

    useEffect - what is Reference?

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
 // useEffect = ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  //title이 바뀔 때만 위의 내용을 trigger
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
+//reference : 우리의 component의 어떤 부분을 선택할 수 있게 하는,
+//document.getElementByID()같은 것
+// react의 모든 component는 reference element를 가지고 있다.
 
 const App = () => {
-  //title의 default값 설정
-  const titleUpdater = useTitle("Loading...");
-  //시간 지연 설정(로딩하고 5초 후에 title이 바뀌게 해보자)
-  setTimeout(() => titleUpdater("home"), 5000);
+  const ref_ex = useRef();
+  //getElementById 역할
+  setTimeout(() => console.log(ref_ex.current), 3000);
+  setTimeout(() => ref_ex.current.focus(), 3000);
 
   return (
     <div className="App">
       <h1> Hooks </h1>
+      <input ref={ref_ex} placeholder="la" />
     </div>
   );
 };
