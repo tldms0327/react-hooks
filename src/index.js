958d15e4097690ee83a56b1c4ce01a69809598d8
tldms0327 <60428730+tldms0327@users.noreply.github.com>
2021-02-15 00:40:59 +0900

    add: useInput ver.2 validator

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 //update an iniput
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
 
   //function
@@ -12,21 +12,29 @@ const useInput = (initialValue) => {
     const {
       target: { value }
     } = event;
-    setValue(value);
+    let willUpdate = true;
+    //validator로서 함수가 들어왔을 때만 동작
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
     console.log(event.target);
   };
   return { value, onChange };
 };
 
 const App = () => {
-  //Mr.가 useInput내로 전달
-  const name = useInput("Mr.");
+  //bool을 리턴하는 함수 정의
+  const maxLen = (value) => value.length <= 10;
+
+  const name = useInput("Mr.", maxLen);
 
   return (
     <div className="App">
       <h1> Hello </h1>
       <h2> Hook </h2>
-      <input placeholder="Name" value={name.value} onChange={name.onChange} />
       {/* ...을 쓰면 원소의 모든 내용을 unpacking */}
       <input placeholder="Name" {...name} />
     </div>
