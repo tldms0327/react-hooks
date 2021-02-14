272db985ea8d1b9625d978dd54268d718aff57a2
tldms0327 <60428730+tldms0327@users.noreply.github.com>
2021-02-15 01:36:11 +0900

    useEffect: introduction

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const content = [
-  {
-    tab: "Section1",
-    content: "content of the section 1"
-  },
-  {
-    tab: "Section2",
-    content: "content of the section 2"
-  }
-];
-
-const useTabs = (initialtab, allTabs) => {
-  //원하는 input이 아닐 땐 kill function
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-
-  const [currentIndex, setCurrentIndex] = useState(initialtab);
-  return {
-    currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex
-  };
-};
+// useEffect = ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate
 
 const App = () => {
-  const { currentItem, changeItem } = useTabs(0, content);
-  console.log("contents:", content);
+  const say1 = () => console.log("say1");
+  const say2 = () => console.log("say2");
+
+  // 아직 변수가 선언되기 전이라 작동하지 않음
+  useEffect(say1, [number]);
+  const [number, setNumber] = useState(0);
+  const [anumber, setaNumber] = useState(0);
+
+  //useEffect(함수, dependency) -> dependency에 선언한 값이 변할 때만 함수 실행 => componentWillUpdate
+  useEffect(say1, [number]);
+  //dependency가 없을 땐 모든 값이 변할 때마다 실행
+  useEffect(say2);
+
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <h1> Hooks </h1>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setaNumber(anumber + 1)}>{anumber}</button>
     </div>
   );
 };
